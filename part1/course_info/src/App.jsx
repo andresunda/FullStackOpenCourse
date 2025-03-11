87c9afe586c2338f9c584549be0b60ef7bc63f6b
andresunda <and_ua@hotmail.com>
2025-03-10 19:19:01 -0600

    Practice 1.1 to 1.5

---
+++ part1/course_info/src/App.jsx
@@ -0,0 +1,67 @@
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises =
+    parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return (
+    <div>
+      <p>Number of exercises {totalExercises}</p>
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <div>
+      <p>
+        {part} {exercises}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
